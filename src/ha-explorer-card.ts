6f437138949c9f981cbcf5255bb2d641c885e006
MVtag <marc@vojer-tagesen.dk>
2026-08-09 08:02:53 +0200

    Use animated canvas in v0.11.0

--- src/ha-explorer-card.ts
+++ src/ha-explorer-card.ts
@@ -1,12 +1,12 @@
 import { LitElement, css, html, nothing } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
-import "./components/explorer-canvas";
+import "./components/explorer-animated-canvas";
 import "./components/explorer-ha-editor";
 import type { ExplorerCardConfig } from "./models/config";
 import type { HomeAssistant } from "./types";
 import { resolvePresences } from "./utils/entity-binding";
 
-const CARD_VERSION = "0.10.5";
+const CARD_VERSION = "0.11.0";
 
 @customElement("ha-explorer-card")
 export class HaExplorerCard extends LitElement {
@@ -61,10 +61,10 @@ export class HaExplorerCard extends LitElement {
             <span>Explorer map</span>
             <h1>${this.config.title}</h1>
           </div>
-          <small>Visual Room Editor · v${CARD_VERSION}</small>
+          <small>Animated Movement · v${CARD_VERSION}</small>
         </header>
 
-        <explorer-canvas
+        <explorer-animated-canvas
           .image=${image}
           .rooms=${rooms}
           .presences=${presences}
@@ -72,7 +72,7 @@ export class HaExplorerCard extends LitElement {
           .maxZoom=${this.config.max_zoom ?? 6}
           .initialZoom=${this.config.initial_zoom ?? 1}
           .fitMode=${this.config.fit_mode ?? "contain"}
-        ></explorer-canvas>
+        ></explorer-animated-canvas>
       </ha-card>
     `;
   }
