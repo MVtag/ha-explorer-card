68ce15cdb154cb39facb75bfe21f62ca2fd6469c
MVtag <marc@vojer-tagesen.dk>
2026-08-14 22:41:33 +0200

    Bump card version to v0.36.1

--- src/ha-explorer-card.ts
+++ src/ha-explorer-card.ts
@@ -6,7 +6,7 @@ import type { ExplorerCardConfig, ExplorerRoom } from "./models/config";
 import type { HomeAssistant } from "./types";
 import { resolvePresences } from "./utils/entity-binding";
 
-const CARD_VERSION = "0.36.0";
+const CARD_VERSION = "0.36.1";
 
 @customElement("ha-explorer-card")
 export class HaExplorerCard extends LitElement {
